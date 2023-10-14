1278da83d7863ef1c5ec604e0aeb9c39d02c4560
ironym <p01048242766@gmail.com>
2023-10-14 11:42:25 +0900

    백준 준비

---
+++ bakjoon/1000.py
@@ -0,0 +1,7 @@
+from sys import stdin
+# stdin = open("bakjoon/input.txt", "r") # 테스트 파일을 읽을 때 사용합니다.
+
+line = stdin.readline() # 한 줄을 읽습니다.
+[nbr1, nbr2] = map(int, line.split(' '))
+
+print(nbr1 + nbr2)
--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print('Hello world')
+x = [1, 2, 3, 4, 5] 
+for nbr in x:
+    print(nbr)
+for nbr in range(1,6):
+    print(nbr) 
+i = 0 
+while i < 5:
+     print(x[i])
+     i +=1
